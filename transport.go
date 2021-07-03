9467a1f1784cfc2ef20be5735db65fb51398a4bc
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2021-07-04 00:48:21 +0600

    track the request making peer
    Signed-off-by: Tamjid Ahmed <mail.to.tamjid@gmail.com>

--- transport.go
+++ transport.go
@@ -5,6 +5,7 @@ import "net"
 // RPC has a command, and provides a Reponse mechanism
 type RPC struct {
 	// Type assert to determine the type
+	Peer net.Addr
 	Command  interface{}
 	RespChan chan<- RPCResponse
 }
